2e14c4b16147924d40f5310cafb404434d04d837
Chloe Park <kpark0119@gmail.com>
2023-09-07 17:26:43 -0400

    works with arrays with destructive and non-destructive methods and operator

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph')
+
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+   const newCat = [...cats]
+   newCat.push(name)
+   return newCat;
+}
+
+function prependCat(name) {
+    const newCat = [name, ...cats];
+    return newCat;
+}
+function removeLastCat() {
+    const newCat = [...cats];
+    newCat.pop();
+    return newCat;
+
+}
+
+function removeFirstCat() {
+    const newCat = [...cats];
+    newCat.shift();
+    return newCat;
+}
